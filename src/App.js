924bb131eb521480d8f247b67366b920dabd1e36
Hellen <hellen.ward@mpb.com>
2022-12-05 19:31:09 +0000

    Got axios working hooray

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
+
+const config = {
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    },
+};
+
+const getData = async () => {
+    try {
+        const url = "/api/images";
+        const response = await axios(url, config);
+        console.log(response.data);
+    } catch (err) {
+        console.error(err.message);
+    }
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+        </div>
+    );
 }
 
 export default App;
