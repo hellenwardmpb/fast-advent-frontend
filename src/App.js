3fb48f6336ebe1d3c85be81b724961e945d5987b
Hellen <hellen.ward@mpb.com>
2022-12-06 06:53:33 +0000

    Initial styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import "./App.css";
-import { useEffect } from "react";
-import { getData } from "./helpers/helpers";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+    const [adventData, setAdventData] = useState();
+
     useEffect(() => {
+        const getData = async () => {
+            try {
+                const url = "/api/images";
+                const config = {
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods":
+                            "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+                    },
+                };
+
+                const response = await axios(url, config);
+
+                setAdventData(response.data);
+            } catch (err) {
+                console.error(err.message);
+            }
+        };
+
         getData();
     }, []);
+
+    console.log({ adventData });
 
     return (
         <div className="App">
