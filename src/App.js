7607b2b67edc7cc4d04e14c28debe4f0afde68f3
Hellen <hellen.ward@mpb.com>
2022-12-06 06:33:17 +0000

    Removed react boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import "./App.css";
 import { useEffect } from "react";
-import axios from "axios";
-
-const config = {
-    headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-    },
-};
-
-const getData = async () => {
-    try {
-        const url = "/api/images";
-        const response = await axios(url, config);
-        console.log(response.data);
-    } catch (err) {
-        console.error(err.message);
-    }
-};
+import { getData } from "./helpers/helpers";
 
 function App() {
     useEffect(() => {
